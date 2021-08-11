ecd851801afe5537b7f584615a2a91fd0e86f7b4
luukee <luke@lukecarlhartman.com>
2021-08-11 06:58:20 -0700

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,16 @@ module.exports = {
     title: "My Gatsby Site",
   },
   plugins: [
-      "gatsby-plugin-gatsby-cloud",
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          name: `blog`,
-          path: `${__dirname}/blog`,
-        }
-      }
-    ],
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
